ad64022ee5309fd9f405a55838454886445de1a1
Konstantin Läufer <laufer@cs.luc.edu>
2017-11-03 09:34:55 -0500

    fixed typo in comment in Main

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -16,7 +16,7 @@ public class Main {
     // 4. create an ArrayList of that size and
     // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
     // 6. sort the ArrayList in descending order by count
-    //    using Arrays.sort and an instance of the provided comparator (after fixing the latter)
+    //    using Collections.sort and an instance of the provided comparator (after fixing the latter)
     // 7. print the (up to) ten most frequent words in the text
 
   }
